e2ac20c68f578630e0b674b6ec4c9d1031d328f4
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-03-27 19:27:28 +0000

    complete calculating and recommending data

--- run.py
+++ run.py
@@ -70,7 +70,7 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating surplus data...\n")
+    print("Calculating Surplus data...\n")
 
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
@@ -98,6 +98,21 @@ def get_last_five_entries_sales():
         columns.append(column[-5:])
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Claculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     """
     Run all program functions
@@ -108,10 +123,13 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automation")
-#main()
+main()
 
-sales_columns = get_last_five_entries_sales()
+
 
 # 1, 2, 3, 4, 5, 6
